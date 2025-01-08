1f0421cb4a6e19fdb74a427d86ad3e5618a6039c
johnykosmos <kumakpospolity@gmail.com>
2025-01-08 21:00:08 +0100

    Fix: Update buffers' data in updateData

--- Engine/graphics/Mesh.cpp
+++ Engine/graphics/Mesh.cpp
@@ -8,9 +8,7 @@ namespace eng {
         layout.push<float>(2);
     }
 
-    Mesh::Mesh(const Vertex vertices[], size_t vertexCount, const unsigned int indices[], size_t indexCount) :
-        vb(VertexBuffer(vertices, vertexCount * sizeof(Vertex))),
-        ib(IndexBuffer(indices, indexCount)){
+    Mesh::Mesh(const Vertex vertices[], size_t vertexCount, const unsigned int indices[], size_t indexCount) {
             layout.push<float>(3);
             layout.push<float>(3);
             layout.push<float>(2);
@@ -23,6 +21,8 @@ namespace eng {
     }
 
     void Mesh::updateData(const Vertex vertices[], size_t vertexCount, const unsigned int indices[], size_t indexCount) {
+        vb.updateData(vertices, vertexCount * sizeof(Vertex));
+        ib.updateData(indices, indexCount);
         va.configureAttributes(vb, layout, false);
         va.addIndexBuffer(ib);    
         va.unbind();
