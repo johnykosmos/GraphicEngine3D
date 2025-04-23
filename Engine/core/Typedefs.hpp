bd8f5a0d2e6b76845b9005fdc94b054e389ea346
johnykosmos <piotrpomaranski97@gmail.com>
2025-04-23 10:38:24 +0200

    Feat: Add ivec types

--- Engine/core/Typedefs.hpp
+++ Engine/core/Typedefs.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "glm/ext/vector_int3.hpp"
 #include <glm/vec3.hpp>
 #include <glm/vec2.hpp>
 #include <glm/mat4x4.hpp>
 
 using Vec3 = glm::vec3;
+using iVec3 = glm::ivec3;
 using Vec2 = glm::vec2;
+using iVec2 = glm::ivec2;
 using Mat4 = glm::mat4;
+
